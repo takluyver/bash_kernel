978fd155903ee055d79f4b9df3c8263276255e10
Thomas Kluyver <takowl@gmail.com>
2014-06-26 17:56:36 -0700

    Add info to setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,23 @@ class install_with_kernelspec(install):
         from IPython.kernel.kernelspec import install_kernel_spec
         install_kernel_spec('kernelspec', 'bash', replace=True)
 
+with open('README.rst') as f:
+    readme = f.read()
+
 setup(name='bash_kernel',
       version='0.1',
       description='A bash kernel for IPython',
+      long_description=readme,
       author='Thomas Kluyver',
       author_email='thomas@kluyver.me.uk',
+      url='https://github.com/takluyver/bash_kernel',
       py_modules=['bash_kernel'],
       cmdclass={'install': install_with_kernelspec},
       install_requires=['ipython>=3.0', 'pexpect>=3.3'],
-      )
+      classifiers = [
+          'Framework :: IPython',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python :: 3',
+          'Topic :: System :: Shells',
+      ]
+)
