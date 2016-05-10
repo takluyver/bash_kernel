0fe4e5b03f0166c968a344eacf604428726b4cf4
zimbatm <zimbatm@zimbatm.com>
2016-05-10 16:12:55 +0100

    Allow to install on --prefix

--- bash_kernel/install.py
+++ bash_kernel/install.py
@@ -1,6 +1,7 @@
 import json
 import os
 import sys
+import getopt
 
 from jupyter_client.kernelspec import install_kernel_spec
 from IPython.utils.tempdir import TemporaryDirectory
@@ -12,7 +13,7 @@ kernel_json = {"argv":[sys.executable,"-m","bash_kernel", "-f", "{connection_fil
  "env":{"PS1": "$"}
 }
 
-def install_my_kernel_spec(user=True):
+def install_my_kernel_spec(user=True, prefix=None):
     with TemporaryDirectory() as td:
         os.chmod(td, 0o755) # Starts off as 700, not user readable
         with open(os.path.join(td, 'kernel.json'), 'w') as f:
@@ -20,7 +21,7 @@ def install_my_kernel_spec(user=True):
         # TODO: Copy resources once they're specified
 
         print('Installing IPython kernel spec')
-        install_kernel_spec(td, 'bash', user=user, replace=True)
+        install_kernel_spec(td, 'bash', user=user, replace=True, prefix=prefix)
 
 def _is_root():
     try:
@@ -29,8 +30,18 @@ def _is_root():
         return False # assume not an admin on non-Unix platforms
 
 def main(argv=[]):
-    user = '--user' in argv or not _is_root()
-    install_my_kernel_spec(user=user)
+    prefix = None
+    user = not _is_root()
+
+    opts, _ = getopt.getopt(args, '', ['--user', '--prefix='])
+    for k, v in opts:
+        if k == '--user':
+            user = True
+        elif k == '--prefix':
+            prefix = v
+            user = False
+
+    install_my_kernel_spec(user=user, prefix=prefix)
 
 if __name__ == '__main__':
     main(argv=sys.argv)
