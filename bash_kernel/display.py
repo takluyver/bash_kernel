629d3f6ec66eb35b8d765634f7d7f4f90e3939e5
jans-code <7jantb@gmx.de>
2024-12-23 15:03:53 +0100

    replace deprecated stdlib imghdr with filetype

--- bash_kernel/display.py
+++ bash_kernel/display.py
@@ -52,10 +52,11 @@ display_data_for_<new_type>; (3) Create an entry in CONTENT_DATA_PREFIXES. Btw, 
 is your friend to debug the format of the content message.
 """
 import base64
-import imghdr
 import json
 import os
 import re
+
+import filetype
 
 
 _TEXT_SAVED_IMAGE = "bash_kernel: saved image data to: "
@@ -101,8 +102,7 @@ def display_data_for_image(filename):
     with open(filename, 'rb') as f:
         image = f.read()
     _unlink_if_temporary(filename)
-
-    image_type = imghdr.what(None, image)
+    image_type = filetype.image_match(image).extension
     if image_type is None:
         raise ValueError("Not a valid image: %s" % image)
 
